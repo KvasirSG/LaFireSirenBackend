b874abd625d9d71b721388133fd8d3daead2a2bc
KvasirSG <12102444+KvasirSG@users.noreply.github.com>
2025-06-12 08:37:43 +0100

    Initial commit

---
+++ src/main/java/kenn767r/lafiresirenbackend/LaFireSirenBackendApplication.java
@@ -0,0 +1,13 @@
+package kenn767r.lafiresirenbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaFireSirenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaFireSirenBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kenn767r/lafiresirenbackend/LaFireSirenBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package kenn767r.lafiresirenbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaFireSirenBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
